26157f00083ac85fbfb64f631f59cd8b3d09764f
Ashok Gelal <ashokgelal@gmail.com>
2014-05-17 22:42:46 -0600

    Add sample project.

---
+++ Puppy.Sample/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿#region Usings
+
+using System.Windows;
+using PuppyFramework.Bootstrap;
+
+#endregion
+
+namespace Puppy.Sample
+{
+    public partial class App
+    {
+        #region Methods
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            new BootstrapConfig
+            {
+                EnableSoftwareUpdater = true,
+                EnableMenuService = true,
+            }.Boot();
+            // OR
+            // new PuppyBootstrapper().Run(config);
+        }
+
+        #endregion
+    }
+}
---
+++ Puppy.Sample/ViewModel/SampleShellViewModel.cs
@@ -0,0 +1,23 @@
+﻿#region Usings
+
+using PuppyFramework.Interfaces;
+using PuppyFramework.ViewModels;
+using System.ComponentModel.Composition;
+
+#endregion
+
+namespace Puppy.Sample.ViewModel
+{
+    [Export(typeof(IPuppyShellViewModel))]
+    internal class SampleShellViewModel : PuppyShellViewModel
+    {
+        #region Constructors
+
+        public SampleShellViewModel()
+        {
+            Title = "Sample Puppy";
+        }
+
+        #endregion
+    }
+}
