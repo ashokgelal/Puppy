cfbe4161be6ef106d8e8cb5f141de0a57abd2c21
Ashok Gelal <ashokgelal@gmail.com>
2014-06-02 01:57:46 -0600

    Log message when a concrete IModuleLoader is found.

--- Puppy/Bootstrap/PuppyModuleInitializer.cs
+++ Puppy/Bootstrap/PuppyModuleInitializer.cs
@@ -20,8 +20,22 @@ namespace PuppyFramework.Bootstrap
 
         private readonly ILoggerFacade _loggerFacade;
         private readonly DevNullModule _devNullModule;
-        [Import(AllowDefault = true)]
         private IModuleLoader _moduleLoader;
+
+        #endregion
+
+        #region Properties
+
+        [Import(AllowDefault = true)]
+        private IModuleLoader ModuleLoader
+        {
+            get { return _moduleLoader; }
+            set
+            {
+                _moduleLoader = value;
+                _loggerFacade.Log(string.Format("ModuleLoader {0} will be used to determine whether a module can be added or not", ModuleLoader.GetType().Name), Category.Info, Priority.Medium);
+            }
+        }
 
         #endregion
 
@@ -42,7 +56,7 @@ namespace PuppyFramework.Bootstrap
 
         protected override IModule CreateModule(ModuleInfo moduleInfo)
         {
-            if (_moduleLoader == null || _moduleLoader.ShouldAllowNonPuppyModule())
+            if (ModuleLoader == null || ModuleLoader.ShouldAllowNonPuppyModule())
             {
                 return base.CreateModule(moduleInfo);
             }
@@ -61,12 +75,12 @@ namespace PuppyFramework.Bootstrap
                 _loggerFacade.Log(string.Format("Module {0} found but it isn't decorated with PuppyModuleExportAttribute. This module won't be created and initialized.", moduleInfo.ModuleName), Category.Warn, Priority.High);
                 return _devNullModule;
             }
-            if (_moduleLoader.ShouldLoadModuleWithAttribute(exportAttribute))
+            if (ModuleLoader.ShouldLoadModuleWithAttribute(exportAttribute))
             {
-                _loggerFacade.Log(string.Format("ModuleLoader {0} accepted to load module {1}.", _moduleLoader.GetType().Name, moduleInfo.ModuleName), Category.Info, Priority.Medium);
+                _loggerFacade.Log(string.Format("{0} has been accepted.", moduleInfo.ModuleName), Category.Info, Priority.Medium);
                 return base.CreateModule(moduleInfo);
             }
-            _loggerFacade.Log(string.Format("ModuleLoader {0} denied to load module {1}. This module won't be created and initialized.", _moduleLoader.GetType().Name, moduleInfo.ModuleName), Category.Warn, Priority.High);
+            _loggerFacade.Log(string.Format("{0} has been denied. This module won't be created and initialized.", moduleInfo.ModuleName), Category.Warn, Priority.High);
             return _devNullModule;
         }
 
