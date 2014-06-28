99f2b8f1e1f2d2f08eaf916e6e96740a6706daff
Ashok Gelal <ashokgelal@gmail.com>
2014-06-28 01:43:30 -0600

    Add an extension method to focus an element.

---
+++ Puppy/Helpers/UIHelpers.cs
@@ -0,0 +1,21 @@
+﻿#region Using
+
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+
+#endregion
+
+namespace PuppyFramework.Helpers
+{
+    public static class FocusHelper
+    {
+        public static void DoFocus(this UIElement element)
+        {
+            if (!element.Focus())
+            {
+                element.Dispatcher.BeginInvoke(DispatcherPriority.Input, new ThreadStart(()=> element.Focus()));
+            }
+        }
+    }
+}
