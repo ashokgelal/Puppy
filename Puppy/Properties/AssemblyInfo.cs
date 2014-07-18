0e63e7d7b224248a1012419cb60710c77a2d5bf5
Ashok Gelal <ashokgelal@gmail.com>
2014-07-18 00:25:24 -0600

    Bump version.

--- Puppy/Properties/AssemblyInfo.cs
+++ Puppy/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Puppy")]
-[assembly: AssemblyCopyright("Ashok Gelal Copyright ©  2014")]
+[assembly: AssemblyCopyright("Ashok Gelal Copyright ©  2014 Ashok Gelal")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.2")]
+[assembly: AssemblyFileVersion("0.0.2.2")]
